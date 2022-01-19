ae9641800c4b517a04116e575b75275230e40927
qingjie <10538315+qingjie@users.noreply.github.com>
2022-01-19 13:49:39 -0500

    Update 3-subnets.tf

--- terraform/3-subnets.tf
+++ terraform/3-subnets.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "private-us-east-1a" {
   vpc_id         = aws_vpc.main.id
-  cird_block    = "10.0.0.0/19"
+  cidr_block    = "10.0.0.0/19"
   availability_zone = "us-east-1a"
   
   tags = {
@@ -12,7 +12,7 @@ resource "aws_subnet" "private-us-east-1a" {
 
 resource "aws_subnet" "private-us-east-1b" {
   vpc_id = aws_vpc.main.id
-  cird_block = "10.0.32.0/19"
+  cidr_block = "10.0.32.0/19"
   availability_zone = "us-east-1b"
 
   tags = {
@@ -24,7 +24,7 @@ resource "aws_subnet" "private-us-east-1b" {
 
 resource "aws_subnet" "public-us-east-1a" {
   vpc_id = aws_vpc.main.id
-  cird_block = "10.0.64.0/19"
+  cidr_block = "10.0.64.0/19"
   availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
 
